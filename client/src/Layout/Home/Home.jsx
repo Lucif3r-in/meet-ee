7930e5025835d2b17500036fb8099f003adcff75
Lucif3r-in <ashutosh123rath@gmail.com>
2023-07-13 21:53:55 +0530

    feat: add files from template

---
+++ client/src/Layout/Home/Home.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import "./Home.css";
+import axios from "axios";
+import { PropagateLoader } from "react-spinners";
+// Components
+import StudentBox from "../../components/Student/Student";
+import SearchStudents from "../../components/SearchStudent/SearchStudents";
+
+const Home = () => {
+  const [data, setData] = useState(null);
+  const [allStudents, setAllStudents] = useState(null);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const students = await axios("/api/students/");
+        setData(students.data);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const removeStudent = async (id) => {
+    try {
+      await axios.delete(`/api/students/${id}`);
+      const students = await axios("/api/students/");
+      setData(students.data);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const searchStudents = (username) => {
+    let allStudentsCopy = [...data.students];
+    if (allStudents === null) setAllStudents(allStudentsCopy);
+
+    let students = data.students.filter(({ name }) =>
+      name.toLowerCase().includes(username.toLowerCase())
+    );
+    if (students.length > 0) setData({ students });
+
+    if (username.trim() === "") setData({ students: allStudents });
+  };
+
+  let students;
+
+  if (data) {
+    students =
+      data.students &&
+      data.students.map((student) => (
+        <StudentBox
+          key={student._id}
+          {...student}
+          removeStudent={removeStudent}
+        />
+      ));
+  } else {
+    return (
+      <div className="Spinner-Wrapper">
+        <PropagateLoader color={"#333"} />
+      </div>
+    );
+  }
+
+  if (error) return <h1>{error}</h1>;
+  if (data !== null && !data.students.length)
+    return <h1 className="No-Students">No students!</h1>;
+
+  return (
+    <div className="Table-Wrapper">
+      <h1>Students:</h1>
+      <SearchStudents searchStudents={searchStudents} />
+      <div className="Grid-Container">{students}</div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/src/components/Student/Student.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Student.css";
+import { Link } from "react-router-dom";
+import Avatar from "react-avatar";
+
+const Student = ({ _id, name, email, enrollnumber, removeStudent }) => {
+  return (
+    <div className="Student-Box">
+      <div className="Student-Box-Content">
+        <p className="Student-Box-Name">{name}</p>
+        <p className="Student-Box-Email">{email}</p>
+        <p className="Student-Box-Enroll">{enrollnumber}</p>
+      </div>
+      <div className="Student-Box-Actions">
+        <button
+          onClick={() => removeStudent(_id)}
+          className="Action-Button fa fa-trash"
+        ></button>
+        <Link to={{ pathname: "/edit", search: _id }}>
+          <button className="Action-Button fa fa-pencil"></button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Student;
